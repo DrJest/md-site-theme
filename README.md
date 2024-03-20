ac64a14090d4f7b594c4af65a8430f70702c9e92
Simone Albano <simone@drjest.dev>
2024-03-20 15:37:42 +0000

    aggiunto rate limiting per le API + README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # md-site-theme
 Custom Wordpress theme for MissionDay.site
+
+## API Endpoints
+API endpoints are available at `/wp-json/wp/v2/mission-day` and include the following:
+
+- `/wp-json/wp/v2/mission-day` - Search Mission Days
+- `/wp-json/wp/v2/mission-day/{id}` - Get a single Mission Day
+
+### Search Mission Days By Date, Area or Type
+You can use `mdq` parameter to search for Mission Days by date, area or type. The parameters is a JSON object with the following properties:
+
+- `date` - Date of the Mission Day (YYYYMMDD)
+- `area` - Area of the Mission Day (emea, apac, amer)
+- `type` - Type of the Mission Day (normal, lite, anomaly)
+- `city` - City of the Mission Day
+
+#### Examples
+- `/wp-json/wp/v2/mission-day?mdq={"date":{"<":"20240131"}}` - Get all Mission Days before 2024-01-31
+- `/wp-json/wp/v2/mission-day?mdq={"area":["emea"],"type":"lite"}` - Get all Mission Days in EMEA that are Lite
+- `/wp-json/wp/v2/mission-day?mdq={"city":"London"}` - Get all Mission Days in London
+- `/wp-json/wp/v2/mission-day?mdq={"area":["emea","amer"],"type":"lite"}` - Get all Mission Days in EMEA that are Lite
+
+If the value is an array, it will be treated as an IN query. If the value is an object, it will be treated as a comparison query.
--- functions.php
+++ functions.php
@@ -1,77 +1,158 @@
 <?php
 
+defined('WPINC') || die();
+
 require_once('widgets/mission-day-map.widget.php');
 require_once('widgets/mission-day-marker.widget.php');
 require_once('widgets/mission-day-calendar.widget.php');
 
-/**
- * Recommended way to include parent theme styles.
- * (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
- *
- */
+const MDSITE_SECONDS_BETWEEN_GUEST_API_CALLS = 1;
+
+const MDSITE_RATE_LIMITED_IPS = array();
+
+const MDSITE_NEVER_RATE_LIMITED_IPS = array(
+  '127.0.0.1',
+  '::1'
+);
 
 add_action('wp_enqueue_scripts', 'hello_elementor_child_style');
 function hello_elementor_child_style()
 {
-	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+
+  wp_enqueue_style('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css');
+  wp_enqueue_script('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js');
 }
 
 add_filter('acf/fields/google_map/api', function ($api) {
-	$api['key'] = get_field('google_api_key', 'option');
-	return $api;
+  $api['key'] = get_field('google_api_key', 'option');
+  return $api;
 });
 
-// Register widget for MissionDay post type, showing the map
 function register_missionday_widget()
 {
-	register_widget('MissionDayMap_Widget');
-	register_widget('MissionDayMarker_Widget');
-	register_widget('MissionDayCalendar_Widget');
+  register_widget('MissionDayMap_Widget');
+  register_widget('MissionDayMarker_Widget');
+  register_widget('MissionDayCalendar_Widget');
 }
 add_action('widgets_init', 'register_missionday_widget');
 
 add_filter('rest_mission-day_query', 'filter_md', 999, 2);
 function filter_md($args, $request)
 {
-	if (!isset($request['mdq'])) {
-		return $args;
-	}
+  if (!isset($request['mdq'])) {
+    return $args;
+  }
 
-	$mdq_value = json_decode($request['mdq']);
+  $mdq_value = json_decode($request['mdq']);
 
-	foreach ($mdq_value as $key => $value) {
-		if (is_array($value)) {
-			$args['meta_query'][] = array(
-				'key' => $key,
-				'value' => $value,
-				'compare' => 'IN',
-			);
-			continue;
-		}
-		if (is_object($value)) {
-			foreach ($value as $k => $v) {
-				$args['meta_query'][] = array(
-					'key' => $key,
-					'value' => $v,
-					'compare' => $k,
-				);
-			}
-			continue;
-		}
-		$args['meta_query'][] = array(
-			'key' => $key,
-			'value' => $value,
-			'compare' => 'LIKE',
-		);
-	}
+  foreach ($mdq_value as $key => $value) {
+    if (is_array($value)) {
+      $args['meta_query'][] = array(
+        'key' => $key,
+        'value' => $value,
+        'compare' => 'IN',
+      );
+      continue;
+    }
+    if (is_object($value)) {
+      foreach ($value as $k => $v) {
+        $args['meta_query'][] = array(
+          'key' => $key,
+          'value' => $v,
+          'compare' => $k,
+        );
+      }
+      continue;
+    }
+    $args['meta_query'][] = array(
+      'key' => $key,
+      'value' => $value,
+      'compare' => 'LIKE',
+    );
+  }
 
-	return $args;
+  return $args;
 }
 
-add_action('wp_enqueue_scripts', 'md_enqueue_scripts');
-function md_enqueue_scripts()
+function mdsite_client_ip()
 {
-	wp_enqueue_style('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css');
-	wp_enqueue_script('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js');
+  global $mdsite_client_ip;
+
+  if (is_null($mdsite_client_ip)) {
+    $server_vars = array(
+      'HTTP_CLIENT_IP',
+      'HTTP_X_FORWARDED_FOR',
+      'REMOTE_ADDR',
+    );
+
+    foreach ($server_vars as $server_var) {
+      if (!array_key_exists($server_var, $_SERVER)) {
+        // The server variable isn't set - do nothing.
+      } elseif (empty($mdsite_client_ip = filter_var($_SERVER[$server_var], FILTER_VALIDATE_IP))) {
+        // The IP address is not valid - do nothing.
+      } else {
+        // We've got a valid IP address in the global variable $mdsite_client_ip,
+        // so we can "break" out of the foreach(...) loop here.
+        break;
+      }
+    }
+
+    // Make sure we don't leave something like an empty string or "false"
+    // in $mdsite_client_ip
+    if (empty($mdsite_client_ip)) {
+      $mdsite_client_ip = null;
+    }
+  }
+
+  return $mdsite_client_ip;
 }
+
+function mdsite_rest_api_init(WP_REST_Server $wp_rest_server)
+{
+  $is_client_rate_limited = false;
+  $transient_key = null;
+
+  if (empty($client_ip = mdsite_client_ip())) {
+    // We don't know the client's IP address so we probably don't want to do
+    // anything here.
+  } elseif (!empty(MDSITE_NEVER_RATE_LIMITED_IPS) && in_array($client_ip, MDSITE_NEVER_RATE_LIMITED_IPS)) {
+    // Never rate-limit IP addresses in the MDSITE_NEVER_RATE_LIMITED_IPS array.
+  } else {
+    $transient_key = 'mdsite_' . $client_ip;
+    $rate_limited_ips = apply_filters('mdsite_rate_limited_ips', MDSITE_RATE_LIMITED_IPS);
+
+    if (!empty($rate_limited_ips)) {
+      $is_client_rate_limited = in_array($client_ip, $rate_limited_ips);
+    } else {
+      $is_client_rate_limited = !is_user_logged_in();
+    }
+
+    $is_client_rate_limited = (bool)apply_filters('mdsite_is_client_rate_limited', $is_client_rate_limited);
+  }
+
+  if (!$is_client_rate_limited) {
+    // The client is not rate-limited - do nothing
+  } elseif (empty($transient_key)) {
+    // If we couldn't figure out the transient key - do nothing
+  } elseif (empty(get_transient($transient_key))) {
+    $seconds_between_api_calls = intval(apply_filters('mdsite_seconds_between_api_calls', MDSITE_SECONDS_BETWEEN_GUEST_API_CALLS, $client_ip));
+    if ($seconds_between_api_calls > 0) {
+      set_transient(
+        $transient_key,
+        '1',
+        $seconds_between_api_calls
+      );
+    }
+  } else {
+    wp_send_json(
+      array(
+        'clientIp' => $client_ip,
+        'message' => 'Slow down your API calls',
+      ),
+      429
+    );
+  }
+}
+add_action('rest_api_init', 'mdsite_rest_api_init', 10, 1);
